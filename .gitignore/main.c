9c8155f620a361d9f279fc06cdb20c65c282cd9a
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-01-29 17:41:25 +0300

    Add files via upload

---
+++ .gitignore/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,once=1,once2,c=1;
+    float x;
+    once2=1/1;
+     for(a=1;a<=10;a++){
+       if(a==1){
+        x=1;
+       }
+       x=/c;
+       c=x;
+
+       printf("%f,",x);
+
+
+     }
+    return 0;
+}
